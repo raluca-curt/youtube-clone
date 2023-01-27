b982dea63c45a022685e0e53fd3264632e1bad6a
Raluca Curt <ralucaioanacurt@gmail.com>
2023-01-27 17:58:59 +0200

    Implemented Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { logo } from '../utils/constants';
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    justifyContent="space-between"
+    p={2}
+    sx={{ position: 'sticky', backgroundColor: '#000', top: 0}}
+  >
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+
+    <SearchBar />
+  </Stack>
+)
 
 export default Navbar
