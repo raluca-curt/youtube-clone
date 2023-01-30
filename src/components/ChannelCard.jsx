288320cb7e2269007fadcf657ed526677cf2289d
Raluca Curt <ralucaioanacurt@gmail.com>
2023-01-30 19:44:17 +0200

    Implemented ChannelCard for Videos

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const ChannelCard = (item) => {
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{ 
+        boxShadow: 'none', 
+        borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '320px' },
+        height: '326px',
+        margin: 'auto'
+      }}
+    >
+      {/* Link to channel */}
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
+          {/* Display channel profile picture */}
+          <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}/>
+
+          {/* Display channel title */}
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: '14px', color: 'gray', ml: '5px'}} />
+          </Typography>
+
+          {/* If statistics are accessible, display subscriber count */}
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            </Typography>
+          )}
+        </CardContent>
+        
+      </Link>
+    </Box>
   )
 }
 
