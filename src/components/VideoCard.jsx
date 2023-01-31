5e6e7914123152cfce09d9e5a61bb50f1284597f
Raluca Curt <ralucaioanacurt@gmail.com>
2023-01-31 18:35:26 +0200

    Additional style changes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   console.log(snippet)
   return (
-    <Card sx={{ width: { xs: '100%', md: '320px' }, boxShadow: 'none', borderRadius: 0, maxWidth: {xs: '358px'} }} className="aloo">
+    <Card sx={{ width: { xs: '100%', md: '320px' }, boxShadow: 'none', borderRadius: 0, maxWidth: {xs: '358px'}, minWidth: '320px' }}>
       {/* Link to video id */}
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
