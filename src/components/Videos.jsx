79203965f2f5f06fd4a2d1db5524b1d27f14d5fd
Raluca Curt <ralucaioanacurt@gmail.com>
2023-01-31 17:13:44 +0200

    Implemented VideoDetail

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,11 @@
 import { Stack, Box } from "@mui/material";
 import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({ videos, justifyContent }) => {
+const Videos = ({ videos, justifyContent, direction }) => {
+  if(!videos?.length) return 'Loading...';
   
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent={justifyContent} gap={2}>
+    <Stack direction={ direction || "row" } flexWrap="wrap" justifyContent={justifyContent || "start" } gap={2}>
 
       {videos.map((item, index) => (
         <Box key={index}>
